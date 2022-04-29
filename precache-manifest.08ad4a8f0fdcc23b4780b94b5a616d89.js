d075e827c72f6e74e077e867595d86c9d4867bdb
maurorobledo87 <maurosrobledo@gmail.com>
2022-04-29 17:24:16 -0300

    Updates

---
+++ precache-manifest.08ad4a8f0fdcc23b4780b94b5a616d89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aa77cbcd8e7c8dc2519",
+    "url": "/sportshirtRobledo/static/css/main.665c1cad.chunk.css"
+  },
+  {
+    "revision": "0aa77cbcd8e7c8dc2519",
+    "url": "/sportshirtRobledo/static/js/main.0aa77cbc.chunk.js"
+  },
+  {
+    "revision": "aa1ecd07d8b85afa671e",
+    "url": "/sportshirtRobledo/static/js/1.aa1ecd07.chunk.js"
+  },
+  {
+    "revision": "cfc66b6d6f0566aef827",
+    "url": "/sportshirtRobledo/static/js/2.cfc66b6d.chunk.js"
+  },
+  {
+    "revision": "4706037cd008e4e9f352",
+    "url": "/sportshirtRobledo/static/js/runtime~main.4706037c.js"
+  },
+  {
+    "revision": "71434506103c9e8dc733350c4dd1c905",
+    "url": "/sportshirtRobledo/static/media/logo.71434506.png"
+  },
+  {
+    "revision": "48b420e56b455b67c4bd5d256f3ab57e",
+    "url": "/sportshirtRobledo/index.html"
+  }
+];
