f5b4f7e1b2d8b9bd36889500c08f6bc11accbfcc
Mauro Robledo <maurosrobledo@gmail.com>
2022-07-07 16:17:52 +0200

    firebase upd

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyDkmti1Ba4VnZ_4B32iwtTBugL9JGDXQ",
-  authDomain: "sportshirt-96685.firebaseapp.com",
-  projectId: "sportshirt-96685",
-  storageBucket: "sportshirt-96685.appspot.com",
-  messagingSenderId: "768077253825",
-  appId: "1:768077253825:web:8f0be0e4f51375144ed840"
+  apiKey: "AIzaSyDvpxbwW02zJhSqng35pdwM1OmSaLjMamk",
+  authDomain: "sportshirts-49ec0.firebaseapp.com",
+  projectId: "sportshirts-49ec0",
+  storageBucket: "sportshirts-49ec0.appspot.com",
+  messagingSenderId: "48037932857",
+  appId: "1:48037932857:web:77d1d952433359a71802e8",
+  measurementId: "G-ZH6F9Z2VJB"
 };
 
 const app = initializeApp(firebaseConfig);
